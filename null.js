5e4aedb79dfbb1f1f108ce07a0751ee5ca775ec6
unknown <oraa-dev>
2025-04-03 18:19:56 +0600

    first commit

---
+++ null.js
@@ -0,0 +1,17 @@
+var nameCro = null ;
+var name = 'sammy'
+console.log(nameCro);
+console.log(name);
+
+
+var orangePrice = 20;
+
+orangePrice +=10
+
+console.log(orangePrice);
+
+var mangoPrice = 40;
+
+mangoPrice = mangoPrice - 10 ;
+
+console.log(mangoPrice);
